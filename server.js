77348f6e0e889b591ffacbb9df643f1f473f3d5e
imperfct2004 <your-email@example.com>
2025-03-13 02:55:19 +0530

    Initial backend commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
